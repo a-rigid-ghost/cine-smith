f1b010eba313d419a4368002a0c628f18c3d58b2
a-rigid-ghost <dastidarag01@gmail.com>
2024-04-04 19:55:58 +0100

    Add Audio length calculation

--- main.py
+++ main.py
@@ -5,20 +5,40 @@ from text_generator.constants import OPENAI_API_KEY
 
 from audio_generator.text_to_speech import convert_text_to_speech
 
+from mutagen.wave import WAVE 
+
 # Login -> https://platform.openai.com/api-keys -> Generate Key -> Replace here
 os.environ[OPENAI_API_KEY] = "<YOUR_API_KEY>"
 IDEA_PROMPT = "Top 10 Places to visit in Schengen Area"
 AUDIO_SPEED = 200
 
 def main():
+    dirname = os.path.dirname(__file__)
+
+    print("Starting Text Generation...")
     # For actual ChatGPT invocation to generate dialogue
     # dialogue = get_dialogue(idea_prompt=IDEA_PROMPT) 
 
     # For Mock dialogue generation
-    dialogue = open(os.path.join(os.path.dirname(__file__), 'data/sample_chatgpt_response.txt'), "r").read()
-    print(dialogue)
+    dialogue = open(os.path.join(dirname, 'data/sample_chatgpt_response.txt'), "r").read()
+
+    print("Text Generation complete.")
+    print(f"Dialogue: {dialogue}")
     
-    filename = f"audio_file_{IDEA_PROMPT}.mp3"
-    convert_text_to_speech(dialogue, AUDIO_SPEED, filename)
+    print("Starting Audio Generation...")
+
+    audio_filename = f"audio_file_{IDEA_PROMPT}.mp3"
+    convert_text_to_speech(dialogue, AUDIO_SPEED, audio_filename)
+
+    print("Audio Generation complete.")
+
+    audio_filepath = os.path.join(dirname, audio_filename)
+    audio = WAVE(audio_filepath)
+    audio_duration = int(audio.info.length)
+
+    print(f"Duration in seconds: {audio_duration}")
+    
+
+
 
 main()
